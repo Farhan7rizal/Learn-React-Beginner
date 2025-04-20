dc4795593c5935144cf6e19d0129505c15a87793
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-04-20 20:06:00 +0700

    Making Components Reusable with Props [Core Concept]

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,21 @@
 import reactImage from "./assets/react-core-concepts.png";
+import componentImage from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max + 1);
+}
+
+function CoreConcept(props) {
+  //this get called in another componen function, and get parameter value," <CoreConcept title="Components" ...." , <= is called props
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -25,6 +37,19 @@ function App() {
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The Core UI building block."
+              image={componentImage}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
