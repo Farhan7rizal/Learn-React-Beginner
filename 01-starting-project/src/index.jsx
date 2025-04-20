3887ce0d633e2834529bc10212752a3641b25df6
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-04-20 19:24:23 +0700

    add some random function logic

--- 01-starting-project/src/index.jsx
+++ 01-starting-project/src/index.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; //responsible output app component (app.jsx)
 
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(<App />); //render is to make jsx file can run html file in our browser
