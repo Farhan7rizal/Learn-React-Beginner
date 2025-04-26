d1444e5c13d7b5ab00a5b784ffc4b5d10fcc1e0a
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-04-26 11:00:39 +0700

    the special "children" prop
    using <TabButton labe="component"> then TabButton({ label }) like children ..................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import componentImage from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
   return (
@@ -22,10 +23,15 @@ function App() {
               image={CORE_CONCEPTS[1].image}
             />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
