37b9f06d4058b146f1612ee9a99c5dbf80475d37
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-04-28 13:06:57 +0700

    Passing Custom Arguments to Event Functions

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,8 +5,9 @@ import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
-  function handleSelect() {
-    console.log("Hello World! - selected");
+  function handleSelect(selectedButton) {
+    //selected button =>'components', 'jsx', 'props' , 'state'
+    console.log("Hello World! - selected " + selectedButton);
   }
 
   return (
@@ -33,10 +34,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
