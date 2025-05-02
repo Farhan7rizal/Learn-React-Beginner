bd65a4e32502d4941e0d0d9737923fba6bd4254a
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-05-02 20:54:18 +0700

    use other than jsx, but so complex

--- 01-starting-project/src/index.jsx
+++ 01-starting-project/src/index.jsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client"; //responsible output app component (app
 
 import App from "./App.jsx";
 import "./index.css";
+import React from "react";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />); //render is to make jsx file can run html file in our browser
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App)); //if don't use jsx
