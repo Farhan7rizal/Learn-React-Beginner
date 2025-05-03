de0eec8698181bddde95975070c07dd727603ea6
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-05-03 20:42:25 +0700

    split components, and forwarded props (2course)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,92 +1,20 @@
 import { useState, Fragment } from "react";
 
-import componentImage from "./assets/components.png";
-import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept";
+import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
+import { CORE_CONCEPTS } from "./data";
 import { EXAMPLES } from "./data";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    //selected button =>'components', 'jsx', 'props' , 'state'
-    // console.log("Hello World! - selected " + selectedButton);
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-
-  let tabContent = <p>Please selected topic</p>;
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
   return (
     <>
       <Header></Header>
-      {EXAMPLES["components"].title}
+
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-          <ul>
-            <CoreConcept
-              title="Components"
-              description="The core UI building block - compose the user interface by combining multiple components."
-              image={componentImage}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          {/* //can't we change children value dynamically based on data.js */}
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === CORE_CONCEPTS[0].title}
-              onSelect={() => handleSelect(EXAMPLES["components"].title)}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts></CoreConcepts>
+        <Examples></Examples>
       </main>
     </>
   );
---
+++ 01-starting-project/src/components/Examples.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { EXAMPLES } from "../data";
+import { CORE_CONCEPTS } from "../data";
+import TabButton from "./TabButton";
+import Section from "./Section";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    //selected button =>'components', 'jsx', 'props' , 'state'
+    // console.log("Hello World! - selected " + selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p>Please selected topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  return (
+    // {/* //can't we change children value dynamically based on data.js */}
+    <Section title="Examples" id="examples">
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === CORE_CONCEPTS[0].title}
+          onClick={() => handleSelect(EXAMPLES["components"].title)}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onClick={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onClick={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onClick={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent}
+    </Section>
+  );
+}
