e5e1497f730a2238972e62135aeee7eb498a99e9
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-05-11 22:33:43 +0700

    Best Practice: Updating Object State Immutably

--- react essentials deep dive/src/components/GameBoard.jsx
+++ react essentials deep dive/src/components/GameBoard.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
+  /*updatedBoard which is a new array where we can use the spread operator to paste in all the existing elements of the old array.So now this is technically a new array object in memory that contains the old array elements as child elements.Now since here I'm dealing with some nest arrays,those should be copied as well before we update anything.And we can achieve this here by calling the map method on our old state, so on our old array here. So that here we got our innerArrays, and for every innerArray we also return a new nested array which spreads the elements of that innerArray inside of it. So that technically here we got a brand new array full of brand new nested arrays which still store the data they did store before, but now it's this updated board that should be updated here
+
+  
+  */
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
