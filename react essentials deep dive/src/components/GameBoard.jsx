8c46a05830def42e1d9e55b0eab78b22c1ad173f
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-05-12 21:01:06 +0700

    Lifting State Up [Core Concept]

--- react essentials deep dive/src/components/GameBoard.jsx
+++ react essentials deep dive/src/components/GameBoard.jsx
@@ -5,7 +5,7 @@ const initialGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -13,9 +13,11 @@ export default function GameBoard() {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare(); //we're executing this function, but the function real executeddefine in the app component, is handleSelectSquare()
   }
 
   /*updatedBoard which is a new array where we can use the spread operator to paste in all the existing elements of the old array.So now this is technically a new array object in memory that contains the old array elements as child elements.Now since here I'm dealing with some nest arrays,those should be copied as well before we update anything.And we can achieve this here by calling the map method on our old state, so on our old array here. So that here we got our innerArrays, and for every innerArray we also return a new nested array which spreads the elements of that innerArray inside of it. So that technically here we got a brand new array full of brand new nested arrays which still store the data they did store before, but now it's this updated board that should be updated here
