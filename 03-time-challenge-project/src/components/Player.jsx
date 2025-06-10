a18193bf16fa905c46d135c900ce67ece101bfa9
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-06-10 20:32:42 +0700

    Using ref

--- 03-time-challenge-project/src/components/Player.jsx
+++ 03-time-challenge-project/src/components/Player.jsx
@@ -1,24 +1,34 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submited, setSubmitted] = useState(false);
+  const playerName = useRef(); //with ref we can get rid one state (enteredPlayerName) and onChange={handleChange}
 
-  function handleChange(event) {
-    //
-    setSubmitted(false); //reset when write name again in input field
-    setEnteredPlayerName(event.target.value);
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submited, setSubmitted] = useState(false);
+
+  // function handleChange(event) {
+  //   //
+  //   setSubmitted(false); //reset when write name again in input field
+  //   setEnteredPlayerName(event.target.value);
+  // }
 
   function handleClick() {
     //
-    setSubmitted(true);
+    // setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome {submited ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input
+          ref={playerName}
+          type="text"
+          // onChange={handleChange}
+          // value={enteredPlayerName}
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
