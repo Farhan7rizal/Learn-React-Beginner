0d55c9e2434ae95cc49b2cc36e626145a9e5bc4e
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-06-10 12:00:30 +0700

    managing user input with state

--- 03-time-challenge-project/src/components/Player.jsx
+++ 03-time-challenge-project/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submited, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    //
+    setSubmitted(false); //reset when write name again in input field
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    //
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submited ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
