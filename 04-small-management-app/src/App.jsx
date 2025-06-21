9747cf672543a3809f9bcde7135738601b2a9239
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-06-21 11:04:51 +0700

    Project 04 Management App
    intial push and make sidebar html

---
+++ 04-small-management-app/src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <main>
+      <ProjectSidebar></ProjectSidebar>
+    </main>
+  );
+}
+
+export default App;
