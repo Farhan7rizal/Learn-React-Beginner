3d49202d1bd57a7776c5a36f4a576136816c2ad9
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-06-24 12:24:17 +0700

    Validating User Input & Showing an Error Modal via useImperativeHandle

--- 04-small-management-app/src/components/NewProject.jsx
+++ 04-small-management-app/src/components/NewProject.jsx
@@ -1,9 +1,12 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
+
 export default function NewProject({ onAdd }) {
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
+  const modal = useRef();
 
   function handleSave() {
     const enteredTitle = title.current.value;
@@ -12,6 +15,15 @@ export default function NewProject({ onAdd }) {
     // need pass data above back to app component
     // ...validation
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      //show error modal
+      modal.current.open();
+      return;
+    }
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,27 +32,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-0 my-4">
-        <li>
-          <button className="px-6 py-2 rounded-md bg-stone-50 text-stone-800 hover:bg-stone-200">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={title} label="Label"></Input>
-        <Input ref={description} label="Description" textarea={true}></Input>
-        <Input ref={dueDate} type="date" label="Due Date"></Input>
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>oops... look like you forget to enter a value.</p>
+        <p>please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-0 my-4">
+          <li>
+            <button className="px-6 py-2 rounded-md bg-stone-50 text-stone-800 hover:bg-stone-200">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Label"></Input>
+          <Input ref={description} label="Description" textarea={true}></Input>
+          <Input ref={dueDate} type="date" label="Due Date"></Input>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
