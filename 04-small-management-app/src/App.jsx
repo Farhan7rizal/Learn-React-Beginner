90db853aced5e1a7e988f7ccd226af62538479cc
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-06-25 23:20:52 +0700

    Handling Project Deletion

--- 04-small-management-app/src/App.jsx
+++ 04-small-management-app/src/App.jsx
@@ -52,13 +52,28 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+          //don't need id because prevState save an id
+        ),
+      };
+    });
+  }
+
   console.log(projectState);
 
   const selectedProject = projectState.projects.find(
     (project) => project.id === projectState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
 
   if (projectState.selectedProjectId === null) {
     content = (
