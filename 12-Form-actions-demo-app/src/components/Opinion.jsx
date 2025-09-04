5a90ce36887234a3bcc0855fa6a68e1bcad5abfc
Farhan7rizal <44233031+Farhan7rizal@users.noreply.github.com>
2025-09-04 21:45:36 +0700

    Adding Optimistic Updating

--- 12-Form-actions-demo-app/src/components/Opinion.jsx
+++ 12-Form-actions-demo-app/src/components/Opinion.jsx
@@ -1,13 +1,21 @@
-import { use, useActionState } from "react";
+import { use, useActionState, useOptimistic } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, mode) => (mode === "up" ? prevVotes + 1 : prevVotes - 1)
+  ); //useOptimistic give instant change to votes number, and provide an temporary instant value +1 / -1, if error back to prev value
+
   async function upvoteAction() {
+    setVotesOptimistically("up");
     await upvoteOpinion(id);
     console.log("UPvote");
   }
   async function downvoteAction() {
+    setVotesOptimistically("down");
     await downvoteOpinion(id);
     console.log("DOWNvote");
   }
@@ -47,7 +55,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button
           formAction={downvoteFormAction}
